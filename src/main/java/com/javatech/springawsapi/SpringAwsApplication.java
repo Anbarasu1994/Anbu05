0504ba457b657a52325c44760277ac636ec308f2
Anbarasu1994 <anbarasukg@gmail.com>
2023-12-06 11:55:26 +0530

    cicd test

--- src/main/java/com/javatech/springawsapi/SpringAwsApplication.java
+++ src/main/java/com/javatech/springawsapi/SpringAwsApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringAwsApplication {
 @GetMapping("/")
 	public String home(){
-		return "!!....testing cicd pipline.....!!";
+		return "!!....Wellcome CICD testing.....!!";
 	}
 	
 
